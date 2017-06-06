cb4d310533e5e36aa798d5160b05f7ba60914a07
gustavoquinalha <gustavo_quinalha@hotmail.com>
2017-06-06 14:02:08 -0300

    fumegando

--- src/main.js
+++ src/main.js
@@ -1,7 +1,28 @@
 import Vue from 'vue'
-import App from './App.vue'
+// import App from './App.vue'
 
 new Vue({
   el: '#app',
-  render: h => h(App)
+  data () {
+    return {
+      site: null,
+      sizes: [
+        {"id": 1, "width": 360, "height": 640},
+        {"id": 2, "width": 480, "height": 800},
+        {"id": 3, "width": 320, "height": 534},
+        {"id": 4, "width": 600, "height": 1024},
+        {"id": 5, "width": 320, "height": 568},
+        {"id": 6, "width": 320, "height": 480},
+        {"id": 7, "width": 375, "height": 667},
+        {"id": 8, "width": 640, "height": 360},
+        {"id": 9, "width": 320, "height": 570},
+        {"id": 10, "width": 540, "height": 960}
+      ]
+    }
+  },
+  methods: {
+      keyUpShow: function (event) {
+      this.site = event.target.value
+      }
+    }
 })
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,19 @@
 var path = require('path')
 var webpack = require('webpack')
+var glob = require('glob');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+let PurifyCSSPlugin = require('purifycss-webpack');
+
+var inProduction = (process.env.NODE_ENV === 'production');
 
 module.exports = {
-  entry: './src/main.js',
+  entry: {
+      app: [
+        './src/main.js',
+        './src/css/main.scss'
+      ]
+    },
+
   output: {
     path: path.resolve(__dirname, './dist'),
     publicPath: '/dist/',
@@ -15,14 +26,17 @@ module.exports = {
         loader: 'vue-loader',
         options: {
           loaders: {
-            // Since sass-loader (weirdly) has SCSS as its default parse mode, we map
-            // the "scss" and "sass" values for the lang attribute to the right configs here.
-            // other preprocessors should work out of the box, no loader config like this necessary.
             'scss': 'vue-style-loader!css-loader!sass-loader',
             'sass': 'vue-style-loader!css-loader!sass-loader?indentedSyntax'
           }
-          // other vue-loader options go here
         }
+      },
+      {
+        test: /\.s[ac]ss$/,
+        use: ExtractTextPlugin.extract({
+          use: ['css-loader', 'sass-loader'],
+          fallback: 'style-loader'
+        })
       },
       {
         test: /\.js$/,
@@ -50,10 +64,23 @@ module.exports = {
   performance: {
     hints: false
   },
-  devtool: '#eval-source-map'
+  devtool: '#eval-source-map',
+
+  plugins: [
+      new ExtractTextPlugin('[name].css'),
+
+      new PurifyCSSPlugin({
+        paths: glob.sync(path.join(__dirname, '**/*.html')),
+        minimize: inProduction
+      }),
+
+      new webpack.LoaderOptionsPlugin({
+        minimize: inProduction
+      })
+    ]
 }
 
-if (process.env.NODE_ENV === 'production') {
+if (inProduction) {
   module.exports.devtool = '#source-map'
   // http://vue-loader.vuejs.org/en/workflow/production.html
   module.exports.plugins = (module.exports.plugins || []).concat([
