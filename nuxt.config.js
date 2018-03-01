9f0d93f424af1eaa5fc91b35ec659f293edf134b
gustavoquinalha <gustavo_quinalha@hotmail.com>
2018-03-01 18:06:24 -0300

    Image preview

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,7 +10,32 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+      { hid: 'description', name: 'description', content: 'A tool for preview changes and test on multiple devices instantly.' },
+      {name: 'author', content: 'Sizify'},
+      {name: 'robots', content: 'index/follow'},
+      {name: 'googlebot', content: 'index/follow'},
+      // google+
+      {itemprop: 'name', content: 'Sizify'},
+      {itemprop: 'description', content: 'A tool for preview changes and test on multiple devices instantly..'},
+      {itemprop: 'image', content: 'https://gustavoquinalha.github.io/sizify/assets/images/intro.jpg'},
+      // facebook
+      {property: 'og:type', content: 'website'},
+      {property: 'og:title', content: 'Sizify'},
+      {property: 'og:url', content: 'https://gustavoquinalha.github.io/sizify/'},
+      {property: 'og:site_name', content: 'Sizify'},
+      {property: 'og:image', content: 'https://gustavoquinalha.github.io/sizify/assets/images/intro.jpg'},
+      {property: 'og:description', content: 'A tool for preview changes and test on multiple devices instantly.'},
+      {property: 'og:locale', content: 'pt_Br'},
+      {property: 'fb:app_id', content: '122670998365518'},
+      {property: 'fb:admins', content: '111111'},
+      // twitter
+      {name: 'twitter:card', content: 'Summary'},
+      {name: 'twitter:title', content: 'Sizify'},
+      {name: 'twitter:description', content: 'A tool for preview changes and test on multiple devices instantly.'},
+      {name: 'twitter:creator', content: 'Sizify'},
+      {name: 'twitter:image', content: 'https://gustavoquinalha.github.io/sizify/assets/images/intro.jpg'},
+      // android
+      {name: 'theme-color', content: '#24292e'}
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
